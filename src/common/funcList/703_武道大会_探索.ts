983c2f554505e3192ee591fe9ae36dac4c18c7ca
709171323 <709171323@qq.com>
2026-08-07 14:01:21 +0800

    feat: 新增703武道大会-探索

---
+++ src/common/funcList/703_武道大会_探索.ts
@@ -0,0 +1,85 @@
+import { IFuncOrigin, IFuncOperatorOrigin, IFuncOperator } from '@/interface/IFunc';
+import { Script } from '@/system/script';
+const left = 0;
+// const center = 1;
+const right = 2;
+
+export class Func703 implements IFuncOrigin {
+	id = 703;
+	name = '武道大会-探索';
+	desc = '武道大会探索界面：已探索出怪物则点击左侧怪物图标进入挑战界面，未探索则点击右侧探索按钮，探索后自动进入怪物挑战界面';
+	operator: IFuncOperatorOrigin[] = [{
+		// 0 检测_怪物挑战界面（仅比色，不点击）
+		desc: [1280, 720,
+			[
+				[left, 80, 614, 0xdcdcdc],
+				[left, 168, 611, 0xfec78d],
+				[right, 1118, 628, 0xff9035],
+				[right, 1141, 575, 0xe5dac3],
+				[right, 1096, 29, 0xff9531],
+				[right, 746, 619, 0xe9f4ff],
+			]
+		]
+	}, {
+		// 1 探索界面_已探索出怪物 → 点击左侧怪物图标
+		desc: [1280, 720,
+			[
+				[right, 894, 36, 0xf8e2b6],
+				[right, 896, 20, 0xfbeecf],
+				[right, 1097, 19, 0xf7d099],
+				[right, 1096, 38, 0xf9edae],
+				[left, 260, 83, 0xa92b36], // 怪物名牌，未探索时不存在
+				[right, 1148, 624, 0xb4a88d],
+				[right, 1169, 623, 0x3b3a35],
+			]
+		],
+		oper: [
+			[left, 1280, 720, 21, 87, 283, 191, 1500], // 点击 左侧怪物图标
+		]
+	}, {
+		// 2 探索界面_未探索 → 点击右侧探索按钮
+		desc: [1280, 720,
+			[
+				[right, 898, 21, 0xfee7bd],
+				[right, 892, 37, 0xf5ddac],
+				[right, 1100, 14, 0xecdba0],
+				[right, 1092, 46, 0xeecc7d],
+				[right, 1146, 594, 0xddd2bb],
+				[right, 1002, 654, 0xfef8df],
+				[left, 128, 339, 0x84868f], // 灰色妖怪头像(尚未发现)，已探索时不存在
+			]
+		],
+		oper: [
+			[right, 1280, 720, 1099, 537, 1191, 645, 2000], // 点击 右侧探索按钮
+		]
+	}];
+	operatorFunc(thisScript: Script, thisOperator: IFuncOperator[]): boolean {
+		// 已经在怪物挑战界面，本功能不再操作，交给方案里后续功能处理
+		if (thisScript.oper({
+			id: 703,
+			name: '武道大会_已在挑战界面',
+			operator: [{ desc: thisOperator[0].desc, notForCnt: true }]
+		})) {
+			return false;
+		}
+		// 已探索出怪物：点击左侧怪物图标进入挑战界面
+		if (thisScript.oper({
+			id: 703,
+			name: '武道大会_点击怪物图标',
+			operator: [thisOperator[1]]
+		})) {
+			return true;
+		}
+		// 未探索：点击右侧探索按钮，探索后游戏会自动进入怪物挑战界面
+		if (thisScript.oper({
+			id: 703,
+			name: '武道大会_点击探索',
+			operator: [thisOperator[2]]
+		})) {
+			return true;
+		}
+		return false;
+	}
+}
+
+export default new Func703();
